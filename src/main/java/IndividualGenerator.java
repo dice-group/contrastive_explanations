a7e9a74141d2732f7fd90bca8a3be8dd5ce2e771
Balram <balramt02@gmail.com>
2025-01-07 13:09:32 +0100

    adding for vrsion2

--- src/main/java/IndividualGenerator.java
+++ src/main/java/IndividualGenerator.java
@@ -3,7 +3,6 @@ import org.semanticweb.owlapi.model.OWLNamedIndividual;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
