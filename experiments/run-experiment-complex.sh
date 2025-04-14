b645fa6d040b24ee9c6202caa9afccfdca639126
Patrick Koopmann <p.k.koopmann@vu.nl>
2025-04-14 16:57:47 +0200

    ..

--- experiments/run-experiment-complex.sh
+++ experiments/run-experiment-complex.sh
@@ -7,6 +7,4 @@ do
     echo $i
     #timeout 600 java -cp contrastive-explanations-0.3-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.contrastive.experiments.ExperimenterWithClassExpressions ~/Git/Data/ore2015_sample/pool_sample/files/$i 7 1000 &> $i.log
     timeout 600 java  -Dlogback.configurationFile=logback.xml -cp contrastive-explanations-0.3-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.contrastive.experiments.ExperimenterWithClassExpressions non-redundant-el/$i 5 100 &> $i.log
-    echo > $i.log
-    echo > $i.log
 done
