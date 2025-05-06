ae46dbd38626712a7f1389d7540171237008463e
Patrick Koopmann <p.k.koopmann@vu.nl>
2025-05-06 13:58:31 +0200

    ..

--- experiments/run-experiment-complex-dl.sh
+++ experiments/run-experiment-complex-dl.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-for i in non-redundant-dl/*owl
+for i in `cat dl-corpus-sorted.csv`
 do
     filename=$(basename "$i")
     echo $filename
-    timeout 600 java  -Dlogback.configurationFile=logback.xml -cp contrastive-explanations-0.3-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.contrastive.experiments.ExperimenterWithClassExpressions non-redundant-dl/$filename 5 100 HERMIT &> $filename.log
+    timeout 600 java  -Dlogback.configurationFile=logback.xml -cp contrastive-explanations-0.3-SNAPSHOT-jar-with-dependencies.jar nl.vu.kai.contrastive.experiments.ExperimenterWithClassExpressions ~/Gits/Data/Ontologies/ORE2015/pool_sample/files/$filename 5 100 HERMIT &> $filename.log
 done
