9e11480ed3525dd0644980f2063400d10823f860
Balram <balramt02@gmail.com>
2025-10-01 17:29:57 +0200

    Add  of FactFoilFileWriter

--- experiments/FactFoilFileWriter.bat
+++ experiments/FactFoilFileWriter.bat
@@ -5,7 +5,8 @@ set BASE=E:\Workspace_Dice\contrastive_explanations_3
 set JAR=E:\Workspace_Dice\contrastive_explanations_3\experiments\contrastive-explanations-0.3-SNAPSHOT-jar-with-dependencies.jar
 set CLASSES=%BASE%\target\classes
 
-set INPUT=E:\Workspace_Dice\DataSource\family_json_input.json
+
+set INPUT=E:\Workspace_Dice\contrastive_explanations_3\experiments\family_json_input.json
 
 echo Running FactFoilFileWriter with input %INPUT%...
 java -cp "%CLASSES%;%JAR%" anonymized.contrastive.experiments.FactFoilFileWriter %INPUT%
