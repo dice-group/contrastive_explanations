838e768263eb08745d489e90d53787c6683255ce
ashikmr <ashikmr111@gmail.com>
2026-01-07 19:18:11 +0100

    cleanup

---
+++ representing_contrastive_explanations/src/main/java/validation/constants/ValidationConstants.java
@@ -0,0 +1,16 @@
+package validation.constants;
+
+public class ValidationConstants {
+
+    public static final String FACT_CANNOT_BE_NULL_OR_EMPTY = "Fact cannot be null or empty.";
+
+    public static final String FOIL_CANNOT_BE_NULL_OR_EMPTY = "Foil cannot be null or empty.";
+
+    public static final String QUERY_CANNOT_BE_NULL_OR_EMPTY = "Query cannot be null or empty.";
+
+    public static final String FACT_FOIL_MUST_BE_DIFFERENT = "Fact and foil must be different.";
+
+    public static final String FACT_NOT_FOUND_IN_ONTOLOGY = "Fact not found in ontology: ";
+
+    public static final String FOIL_NOT_FOUND_IN_ONTOLOGY = "Foil not found in ontology: ";
+}
